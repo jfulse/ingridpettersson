7dff9d883d30e703e9027b6f51d671718fd2c0bc
jfulse <jorgen.fulsebakke@gmail.com>
2021-11-15 20:42:52 +0100

    :wrench: Use fully qualified API url

--- webpage/utils/getApiUrl.ts
+++ webpage/utils/getApiUrl.ts
@@ -1,6 +1,8 @@
-const getApiUrl = (): string =>
-  process.env.NEXT_PUBLIC_VERCEL_URL ||
-  process.env.API_URL ||
-  "http://localhost:3000";
+const getApiUrl = (): string => {
+  const { NEXT_PUBLIC_VERCEL_URL } = process?.env ?? {};
+  if (NEXT_PUBLIC_VERCEL_URL) return `https://${NEXT_PUBLIC_VERCEL_URL}`;
+
+  return process.env.API_URL || "http://localhost:3000";
+};
 
 export default getApiUrl;
