32baae363a92f63c1a0cb9c3a3ff82f2c8e590b7
jfulse <jorgen.fulsebakke@gmail.com>
2021-11-15 20:45:48 +0100

    :bug: Attempt to fix env variable

--- webpage/utils/getApiUrl.ts
+++ webpage/utils/getApiUrl.ts
@@ -1,6 +1,6 @@
 const getApiUrl = (): string => {
-  const { NEXT_PUBLIC_VERCEL_URL } = process?.env ?? {};
-  if (NEXT_PUBLIC_VERCEL_URL) return `https://${NEXT_PUBLIC_VERCEL_URL}`;
+  const verceuUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+  if (verceuUrl) return `https://${verceuUrl}`;
 
   return process.env.API_URL || "http://localhost:3000";
 };
