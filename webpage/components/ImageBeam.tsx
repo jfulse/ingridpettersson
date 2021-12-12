0e753b79d3a9929d21f1c11ca18b7f527903e6d9
jfulse <jorgen.fulsebakke@gmail.com>
2021-12-12 14:06:32 +0100

    :lipstick: Center image when there is only one

--- webpage/components/ImageBeam.tsx
+++ webpage/components/ImageBeam.tsx
@@ -6,11 +6,19 @@ import Image from "./Image";
 
 // Check if imagebeam can be taller on e.g. project She gets high
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ singleImage?: boolean }>`
   width: 100%;
   overflow-x: auto;
   white-space: nowrap;
   margin-left: 1.5rem;
+
+  ${({ singleImage }) =>
+    singleImage &&
+    `
+    display: flex;
+    justify-content: center;
+    margin: 0;
+  `}
 `;
 
 type Props = {
@@ -27,7 +35,7 @@ const ImageBeam = ({ imageObjects, maxHeight = 80 }: Props) => {
   const height = useImageHeight(imageObjects?.[0]?.image, maxHeight, MAX_IMAGE_VIEW_WIDTH);
 
   return (
-    <Wrapper>
+    <Wrapper singleImage={imageObjects?.length < 2}>
       {imageObjects?.map(({ image, href }, idx) => (
         <Image key={image?._key ?? idx} image={image} height={height} href={href} gap="1.5rem" />
       ))}
