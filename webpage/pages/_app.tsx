b3b4c2a78a5aa1772331ba02e7284bdaaeadb032
jfulse <jorgen.fulsebakke@gmail.com>
2022-01-10 21:40:30 +0100

    :clown_face: Use test API key in Stripe frontend

--- webpage/pages/_app.tsx
+++ webpage/pages/_app.tsx
@@ -11,13 +11,13 @@ import "./reactImageGallery.css";
 
 // TODO: Create restricted keys
 
-const PUBLIC_STRIPE_API_KEY =
-  "pk_live_51JhIA7KdnxvJBzuhG0dYWydIqDt2FJYTblmvpUDbessM5Z4mZw5fsrGNirbmvUGwoWqB3YpQdrKRNOq6BJB4Hy0200xnT3bf2K";
+// const PUBLIC_STRIPE_API_KEY =
+// "pk_live_51JhIA7KdnxvJBzuhG0dYWydIqDt2FJYTblmvpUDbessM5Z4mZw5fsrGNirbmvUGwoWqB3YpQdrKRNOq6BJB4Hy0200xnT3bf2K";
 
-// const PUBLIC_STRIPE_API_KEY_TEST =
-//   "pk_test_51JhIA7KdnxvJBzuhhBTUibwsBBaRu8VgjBHEhvyz8pI8xaaj7NgcoFVnTX9dJvbHi742wEjaogFiQVcDqT4UpJxq00PZQD7vId";
+const PUBLIC_STRIPE_API_KEY_TEST =
+  "pk_test_51JhIA7KdnxvJBzuhhBTUibwsBBaRu8VgjBHEhvyz8pI8xaaj7NgcoFVnTX9dJvbHi742wEjaogFiQVcDqT4UpJxq00PZQD7vId";
 
-const stripePromise = loadStripe(PUBLIC_STRIPE_API_KEY);
+const stripePromise = loadStripe(PUBLIC_STRIPE_API_KEY_TEST);
 
 function App({ Component, pageProps }: AppProps) {
   return (
